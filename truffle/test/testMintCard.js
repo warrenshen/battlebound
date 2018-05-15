86a3b80866bf70f7c7f1f788e79f873643488adf
warrenshen <warren.shen@houzz.com>
2018-05-14 22:52:02 -0700

    Update test mint cards test with category and add new test for mint cards of multiple templates

--- truffle/test/testMintCard.js
+++ truffle/test/testMintCard.js
@@ -4,97 +4,160 @@ contract('CardTreasury', function(accounts) {
 
   let contract;
 
-  beforeEach(async function() {
-    contract = await CardTreasury.new();
-    contract.createTemplate("T1", 2, { from: accounts[0] });
+  describe ("should nots", function() {
+    beforeEach(async function() {
+      contract = await CardTreasury.new();
+      contract.createTemplate(0, "T1", 2, { from: accounts[0] });
+    });
+
+    it ("should not allow unprivileged address to mint a card", async function() {
+      let transaction = null;
+
+      try {
+        transaction = await contract.mintCard(0, accounts[1], { from: accounts[1] });
+      } catch (error) {
+        err = error
+      }
+      assert.equal(transaction, null, "transaction should not exist");
+
+      const supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 0, "supply of instances is not 0");
+    });
+
+    it ("should not allow a card with invalid template to be minted", async function() {
+      let transaction;
+      let supply;
+
+      transaction = null;
+
+      try {
+        transaction = await contract.mintCard(1, accounts[0], { from: accounts[0] });
+      } catch (error) {
+        err = error
+      }
+      assert.equal(transaction, null, "transaction should not exist");
+
+      supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 0, "supply of templates is not 0");
+    });
+
+    it ("should not allow more cards than mint limit to be minted", async function() {
+      let transaction;
+      let supply;
+
+      await contract.mintCard(0, accounts[0], { from: accounts[0] });
+      await contract.mintCard(0, accounts[0], { from: accounts[0] });
+
+      transaction = null;
+
+      try {
+        transaction = await contract.mintCard(0, accounts[0], { from: accounts[0] });
+      } catch (error) {
+        err = error
+      }
+      assert.equal(transaction, null, "transaction should not exist");
+
+      supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 2, "supply of templates is not 2");
+    });
   });
 
-  it ("should allow privileged address to mint a card for self", async function() {
-    const recipient = accounts[0];
+  describe ("shoulds", function() {
+    beforeEach(async function() {
+      contract = await CardTreasury.new();
+      contract.createTemplate(1, "T1", 2, { from: accounts[0] });
+      contract.createTemplate(2, "T2", 2, { from: accounts[0] });
+    });
 
-    const transaction = await contract.mintCard(0, recipient);
-    assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
-    assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
+    it ("should allow privileged address to mint a card for self", async function() {
+      const recipient = accounts[0];
 
-    const supply = await contract.instancesSupply.call();
-    assert.equal(supply.valueOf(), 1, "supply of instances is not 1");
+      const transaction = await contract.mintCard(0, recipient);
+      assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
+      assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
 
-    const owner = await contract.ownerOf.call(0);
-    assert.equal(owner, recipient, "owner is not correct");
-  });
+      const supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 1, "supply of instances is not 1");
 
-  it ("should allow privileged address to mint a card for other", async function() {
-    const recipient = accounts[1];
+      const owner = await contract.ownerOf.call(0);
+      assert.equal(owner, recipient, "owner is not correct");
+    });
 
-    const transaction = await contract.mintCard(0, recipient);
-    assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
-    assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
+    it ("should allow privileged address to mint a card for other", async function() {
+      const recipient = accounts[1];
 
-    const supply = await contract.instancesSupply.call();
-    assert.equal(supply.valueOf(), 1, "supply of instances is not 1");
+      const transaction = await contract.mintCard(0, recipient);
+      assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
+      assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
 
-    const owner = await contract.ownerOf.call(0);
-    assert.equal(owner, recipient, "owner is not correct");
-  });
+      const supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 1, "supply of instances is not 1");
 
-  it ("should not allow unprivileged address to mint a card", async function() {
-    let transaction = null;
+      const owner = await contract.ownerOf.call(0);
+      assert.equal(owner, recipient, "owner is not correct");
+    });
 
-    try {
-      transaction = await contract.mintCard(0, accounts[1], { from: accounts[1] });
-    } catch (error) {
-      err = error
-    }
-    assert.equal(transaction, null, "transaction should not exist");
+    it ("should allow privileged address to mint multiple cards", async function() {
+      const recipient = accounts[0];
+      let owner;
+      let transaction;
+      let supply;
 
-    const supply = await contract.instancesSupply.call();
-    assert.equal(supply.valueOf(), 0, "supply of instances is not 0");
-  });
+      transaction = await contract.mintCard(0, recipient, { from: accounts[0] });
+      assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
+      assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
 
-  it ("should allow privileged address to mint multiple cards", async function() {
-    const recipient = accounts[0];
-    let owner;
-    let transaction;
-    let supply;
+      supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 1, "supply of instances is not 1");
 
-    transaction = await contract.mintCard(0, recipient, { from: accounts[0] });
-    assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
-    assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
+      transaction = await contract.mintCard(0, recipient, { from: accounts[0] });
+      assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
+      assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
 
-    supply = await contract.instancesSupply.call();
-    assert.equal(supply.valueOf(), 1, "supply of templates is not 1");
+      supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 2, "supply of instances is not 2");
 
-    transaction = await contract.mintCard(0, recipient, { from: accounts[0] });
-    assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
-    assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
+      owner = await contract.ownerOf.call(0);
+      assert.equal(owner, recipient, "owner is not correct");
 
-    supply = await contract.instancesSupply.call();
-    assert.equal(supply.valueOf(), 2, "supply of templates is not 2");
+      owner = await contract.ownerOf.call(1);
+      assert.equal(owner, recipient, "owner is not correct");
+    });
 
-    owner = await contract.ownerOf.call(0);
-    assert.equal(owner, recipient, "owner is not correct");
+    it ("should allow privileged address to mint multiple cards with multiple templates", async function() {
+      let owner;
+      let transaction;
+      let supply;
 
-    owner = await contract.ownerOf.call(1);
-    assert.equal(owner, recipient, "owner is not correct");
-  });
-
-  it ("should not allow more cards than mint limit to be minted", async function() {
-    let transaction;
-    let supply;
-
-    await contract.mintCard(0, accounts[0], { from: accounts[0] });
-    await contract.mintCard(0, accounts[0], { from: accounts[0] });
-
-    transaction = null;
-
-    try {
       transaction = await contract.mintCard(0, accounts[0], { from: accounts[0] });
-    } catch (error) {
-      err = error
-    }
-    assert.equal(transaction, null, "transaction should not exist");
+      assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
+      assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
 
-    supply = await contract.instancesSupply.call();
-    assert.equal(supply.valueOf(), 2, "supply of templates is not 2");
+      supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 1, "supply of instances is not 1");
+
+      transaction = await contract.mintCard(1, accounts[1], { from: accounts[0] });
+      assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
+      assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
+
+      supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 2, "supply of instances is not 2");
+
+      transaction = await contract.mintCard(0, accounts[0], { from: accounts[0] });
+      assert.equal(transaction.logs[0].event, "Transfer", "expected a Transfer event");
+      assert.equal(transaction.logs[1].event, "InstanceMinted", "expected an InstanceMinted event");
+
+      supply = await contract.instancesSupply.call();
+      assert.equal(supply.valueOf(), 3, "supply of instances is not 3");
+
+      owner = await contract.ownerOf.call(0);
+      assert.equal(owner, accounts[0], "owner is not correct");
+
+      owner = await contract.ownerOf.call(1);
+      assert.equal(owner, accounts[1], "owner is not correct");
+
+      owner = await contract.ownerOf.call(2);
+      assert.equal(owner, accounts[0], "owner is not correct");
+    });
   });
 });
