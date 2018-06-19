616d656ffafde13a8f9f13160e48265b31db7daf
warrenshen <warren.shen@houzz.com>
2018-06-18 22:08:21 -0700

    Add card attack attributes data structure

---
+++ game/Assets/Scripts/Data Structures/CardAttackAttributes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class CardAttackAttributes
+{
+    [SerializeField]
+    private int fieldId;
+	public int FieldId => fieldId;
+
+	[SerializeField]
+	private string targetId;
+	public string TargetId => targetId;
+}
