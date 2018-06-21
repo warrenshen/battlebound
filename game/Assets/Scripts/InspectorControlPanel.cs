4dbbf83755ac028c026f3008af8596fab3df520f
warrenshen <warren.shen@houzz.com>
2018-06-20 22:41:14 -0700

    Add button to toggle battle dev tools

---
+++ game/Assets/Scripts/InspectorControlPanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InspectorControlPanel : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject mockMovesPanel;
+	[SerializeField]
+	private Button toggleInspectorButton;
+
+    // Use this for initialization
+    private void Awake()
+    {
+		this.toggleInspectorButton.onClick.AddListener(OnToggleInspectorButtonClick);
+    }
+
+	private void OnToggleInspectorButtonClick()
+	{
+		this.mockMovesPanel.SetActive(!this.mockMovesPanel.active);
+	}
+}
