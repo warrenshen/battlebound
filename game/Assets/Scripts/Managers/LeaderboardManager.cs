a3f37a9b6f1ef14122fc71efb1e3274d7ed45697
warrenshen <warren.shen@houzz.com>
2018-07-28 22:47:38 -0700

    Create leaderboard scene

---
+++ game/Assets/Scripts/Managers/LeaderboardManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+
+public class LeaderboardManager : MonoBehaviour
+{
+
+    public void Awake()
+    {
+
+    }
+
+    private void SendGetLeaderboardRequest()
+    {
+
+    }
+
+    private void OnGetLeaderboardSuccess(LogEventResponse response)
+    {
+        Debug.Log("FindMatch request success.");
+    }
+
+    private void OnGetLeaderboardError(LogEventResponse response)
+    {
+        Debug.LogError("FindMatch request error.");
+    }
+}
