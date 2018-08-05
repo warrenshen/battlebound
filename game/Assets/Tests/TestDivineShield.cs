d72eaf54b697b2e35ba2891d8b8f26fdf1f1f79e
warrenshen <warren.shen@houzz.com>
2018-08-04 23:07:14 -0700

    Create divine shield tests

--- game/Assets/Tests/TestDivineShield.cs
+++ game/Assets/Tests/TestDivineShield.cs
@@ -3,12 +3,12 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-public class TestDeathwish
+public class TestDivineShield
 {
     private const string PLAYER_STATE = @"{
         ""id"": ""ID_PLAYER"",
         ""displayName"": ""Player"",
-        ""hasTurn"": 0,
+        ""hasTurn"": 1,
         ""manaCurrent"": 20,
         ""manaMax"": 20,
         ""health"": 100,
@@ -25,20 +25,21 @@ public class TestDeathwish
                 ""level"": 1,
                 ""category"": 0,
                 ""color"": 1,
-                ""attack"": 20,
-                ""health"": 10,
-                ""cost"": 20,
-                ""name"": ""Firebug Catelyn"",
+                ""attack"": 10,
+                ""health"": 30,
+                ""cost"": 30,
+                ""name"": ""Pricklepillar"",
                 ""description"": """",
                 ""abilities"": [
-                    10
+                    1,
+                    21
                 ],
-                ""attackStart"": 20,
-                ""costStart"": 20,
-                ""healthStart"": 10,
-                ""healthMax"": 10,
+                ""attackStart"": 10,
+                ""costStart"": 30,
+                ""healthStart"": 30,
+                ""healthMax"": 30,
                 ""buffs"": [],
-                ""canAttack"": 0,
+                ""canAttack"": 1,
                 ""isFrozen"": 0,
                 ""spawnRank"": 3
             },
@@ -48,7 +49,7 @@ public class TestDeathwish
                 ""level"": 1,
                 ""category"": 0,
                 ""attack"": 40,
-                ""health"": 20,
+                ""health"": 60,
                 ""cost"": 60,
                 ""name"": ""Adderspine Weevil"",
                 ""description"": ""Taunt; Deathwish: Deal 20 dmg to all opponent creatures"",
@@ -61,7 +62,7 @@ public class TestDeathwish
                 ""healthStart"": 60,
                 ""healthMax"": 60,
                 ""buffs"": [],
-                ""canAttack"": 0,
+                ""canAttack"": 1,
                 ""isFrozen"": 0,
                 ""isSilenced"": 0,
                 ""spawnRank"": 4
@@ -85,7 +86,7 @@ public class TestDeathwish
     private const string ENEMY_STATE = @"{
         ""id"": ""ID_ENEMY"",
         ""displayName"": ""Enemy"",
-        ""hasTurn"": 1,
+        ""hasTurn"": 0,
         ""manaCurrent"": 20,
         ""manaMax"": 20,
         ""health"": 100,
@@ -97,70 +98,33 @@ public class TestDeathwish
         ""hand"": [],
         ""field"": [
             {
-                ""id"": ""ID_ENEMY-3"",
-                ""playerId"": ""ID_ENEMY"",
-                ""level"": 1,
-                ""category"": 0,
-                ""attack"": 20,
-                ""health"": 20,
-                ""cost"": 20,
-                ""name"": ""Young Kyo"",
-                ""description"": """",
-                ""abilities"": [
-                    15
-                ],
-                ""attackStart"": 20,
-                ""costStart"": 20,
-                ""healthStart"": 20,
-                ""healthMax"": 20,
-                ""buffs"": [],
-                ""canAttack"": 1,
-                ""isFrozen"": 0,
-                ""spawnRank"": 0
+                ""id"": ""EMPTY""
             },
             {
-                ""id"": ""ID_ENEMY-4"",
-                ""playerId"": ""ID_ENEMY"",
-                ""level"": 1,
-                ""category"": 0,
-                ""attack"": 20,
-                ""health"": 20,
-                ""cost"": 20,
-                ""name"": ""Young Kyo"",
-                ""description"": """",
-                ""abilities"": [
-                    15
-                ],
-                ""attackStart"": 20,
-                ""costStart"": 20,
-                ""healthStart"": 20,
-                ""healthMax"": 20,
-                ""buffs"": [],
-                ""canAttack"": 1,
-                ""isFrozen"": 0,
-                ""spawnRank"": 1
+                ""id"": ""EMPTY""
             },
             {
-                ""id"": ""ID_ENEMY-5"",
+                ""id"": ""ID_ENEMY-9"",
                 ""playerId"": ""ID_ENEMY"",
                 ""level"": 1,
                 ""category"": 0,
                 ""attack"": 40,
-                ""health"": 30,
-                ""cost"": 20,
-                ""name"": ""Ritual Hatchling"",
+                ""health"": 60,
+                ""cost"": 60,
+                ""name"": ""Temple Guardian"",
                 ""description"": """",
                 ""abilities"": [
-                    24
+                    1,
+                    2
                 ],
                 ""attackStart"": 40,
-                ""costStart"": 20,
-                ""healthStart"": 30,
-                ""healthMax"": 30,
+                ""costStart"": 60,
+                ""healthStart"": 60,
+                ""healthMax"": 60,
                 ""buffs"": [],
-                ""canAttack"": 1,
+                ""canAttack"": 0,
                 ""isFrozen"": 0,
-                ""spawnRank"": 2
+                ""spawnRank"": 7
             },
             {
                 ""id"": ""EMPTY""
@@ -207,64 +171,44 @@ public class TestDeathwish
     }
 
     [UnityTest]
-    public IEnumerator DamageEnemyFaceDeathwishTest()
+    public IEnumerator ShieldBlocksAttackTest()
     {
-        BoardCreature playerCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_PLAYER", "ID_PLAYER-5");
-        BoardCreature enemyCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_ENEMY", "ID_ENEMY-3");
+        BoardCreature playerCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_PLAYER", "ID_PLAYER-12");
+        BoardCreature enemyCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_ENEMY", "ID_ENEMY-9");
 
         EffectManager.Instance.OnCreatureAttack(
-            enemyCreature,
-            playerCreature
+            playerCreature,
+            enemyCreature
         );
 
         yield return null;
 
-        enemyCreature = Board.Instance().GetCreatureByPlayerIdAndIndex("ID_ENEMY", 0);
-        Assert.AreEqual(null, enemyCreature);
+        enemyCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_PLAYER", "ID_PLAYER-12");
+        Assert.AreEqual(20, enemyCreature.Health);
 
-        yield return null;
-
-        PlayerAvatar enemyAvatar = BattleState.Instance().Opponent.Avatar;
-        Assert.AreEqual(90, enemyAvatar.Health);
-
-        yield return null;
-
-        playerCreature = Board.Instance().GetCreatureByPlayerIdAndIndex("ID_PLAYER", 0);
-        Assert.AreEqual(null, playerCreature);
+        playerCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_ENEMY", "ID_ENEMY-9");
+        Assert.AreEqual(false, playerCreature.HasAbility(Card.CARD_ABILITY_SHIELD));
+        Assert.AreEqual(60, playerCreature.Health);
     }
 
     [UnityTest]
-    public IEnumerator DamageAllEnemyCreaturesDeathwishTest()
+    public IEnumerator ShieldBlocksLethalTest()
     {
-        BoardCreature playerCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_PLAYER", "ID_PLAYER-12");
-        BoardCreature enemyCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_ENEMY", "ID_ENEMY-3");
+        BoardCreature playerCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_PLAYER", "ID_PLAYER-5");
+        BoardCreature enemyCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_ENEMY", "ID_ENEMY-9");
 
         EffectManager.Instance.OnCreatureAttack(
-            enemyCreature,
-            playerCreature
+            playerCreature,
+            enemyCreature
         );
 
         yield return null;
 
-        enemyCreature = Board.Instance().GetCreatureByPlayerIdAndIndex("ID_ENEMY", 0);
+        enemyCreature = Board.Instance().GetCreatureByPlayerIdAndIndex("ID_PLAYER", 0);
         Assert.AreEqual(null, enemyCreature);
 
-        yield return null;
-
-        enemyCreature = Board.Instance().GetCreatureByPlayerIdAndIndex("ID_ENEMY", 1);
-        Assert.AreEqual(0, enemyCreature.Health);
-
-        enemyCreature = Board.Instance().GetCreatureByPlayerIdAndIndex("ID_ENEMY", 2);
-        Assert.AreEqual(10, enemyCreature.Health);
-
-        yield return null;
-
-        enemyCreature = Board.Instance().GetCreatureByPlayerIdAndIndex("ID_ENEMY", 1);
-        Assert.AreEqual(null, enemyCreature);
-
-        yield return null;
-
-        playerCreature = Board.Instance().GetCreatureByPlayerIdAndIndex("ID_PLAYER", 1);
-        Assert.AreEqual(null, playerCreature);
+        playerCreature = Board.Instance().GetCreatureByPlayerIdAndCardId("ID_ENEMY", "ID_ENEMY-9");
+        Assert.AreEqual(false, playerCreature.HasAbility(Card.CARD_ABILITY_SHIELD));
+        Assert.AreEqual(60, playerCreature.Health);
     }
 }
