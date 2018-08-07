8c910fef4b06a9941950c9dcef99a9e3d7cc1976
Nick Shen <nickzshen@gmail.com>
2018-08-07 02:48:59 -0700

    Confirmatiom UI window works for MP, stable generics singleinputfield window too

--- game/Assets/Plugins/UMenu Pro/Content/Scripts/Core/UMP_Manager.cs
+++ game/Assets/Plugins/UMenu Pro/Content/Scripts/Core/UMP_Manager.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.Events;
 using System.Collections.Generic;
 
-public class UMP_Manager : MonoBehaviour {
+using Nethereum.Web3.Accounts;
 
+public class UMP_Manager : MonoBehaviour
+{
     [Header("Level Manager")]
     public List<LevelInfo> Levels = new List<LevelInfo>();
 
@@ -19,17 +22,17 @@ public class UMP_Manager : MonoBehaviour {
 
     private int CurrentWindow = -1;
 
-    /// <summary>
-    /// 
-    /// </summary>
+    public UMP_ConfirmationDialogUI ConfirmationDialog;
+    public UMP_InputDialogUI InputFieldDialog;
+
+    public static UMP_Manager Instance { get; private set; }
+
     void Awake()
     {
+        Instance = this;
         InstanceLevels();
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
     void InstanceLevels()
     {
         for (int i = 0; i < Levels.Count; i++)
@@ -37,14 +40,11 @@ public class UMP_Manager : MonoBehaviour {
             GameObject l = Instantiate(LevelPrefab) as GameObject;
 
             UMP_LevelInfo li = l.GetComponent<UMP_LevelInfo>();
-            li.GetInfo(Levels[i].Title, Levels[i].Description, Levels[i].Preview, Levels[i].LevelName,PlayButtonName);
+            li.GetInfo(Levels[i].Title, Levels[i].Description, Levels[i].Preview, Levels[i].LevelName, PlayButtonName);
 
             l.transform.SetParent(LevelPanel, false);
         }
     }
-    /// <summary>
-    /// 
-    /// </summary>
     /// <param name="id">window to active</param>
     /// <param name="disable">disabled currents window?</param>
     public void ChangeWindow(int id)
@@ -58,7 +58,7 @@ public class UMP_Manager : MonoBehaviour {
             {
                 Windows[i].SetActive(false);
             }
-           
+
         }
         CurrentWindow = id;
         Windows[id].SetActive(true);
@@ -70,37 +70,61 @@ public class UMP_Manager : MonoBehaviour {
     /// <param name="url"></param>
     public void SocialButton(string url) { Application.OpenURL(url); }
 
-    /// <summary>
-    /// 
-    /// </summary>
     /// <param name="indexDialog"></param>
     public void ShowDialog(int indexDialog)
-    {
-        if(Dialogs[indexDialog] != null)
-        {
-            Dialogs[indexDialog].gameObject.SetActive(true);
-        }
-        else
-        {
-            Debug.Log(string.Format("Does't exits a dialog in the index {0} of list", indexDialog));
-        }
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="indexDialog"></param>
-    public void ShowDialog(int indexDialog,string text)
     {
         if (Dialogs[indexDialog] != null)
         {
             Dialogs[indexDialog].gameObject.SetActive(true);
-             if (!string.IsNullOrEmpty(text)) { Dialogs[indexDialog].SetText(text); }
         }
         else
         {
-            Debug.Log(string.Format("Does't exits a dialog in the index {0} of list", indexDialog));
+            Debug.Log(string.Format("Doesn't exist a dialog in the index {0} of list", indexDialog));
         }
+    }
+
+    /// <param name="indexDialog"></param>
+    public void ShowDialog(int indexDialog, string text)
+    {
+        if (Dialogs[indexDialog] != null)
+        {
+            Dialogs[indexDialog].gameObject.SetActive(true);
+            if (!string.IsNullOrEmpty(text)) { Dialogs[indexDialog].SetText(text); }
+        }
+        else
+        {
+            Debug.Log(string.Format("Doesn't exist a dialog in the index {0} of list", indexDialog));
+        }
+    }
+
+    public void ShowConfirmationDialog(string title,
+                                       string text,
+                                       UnityAction confirmAction,
+                                       UnityAction cancelAction,
+                                       string cancelText = "Cancel",
+                                       string confirmText = "Confirm")
+    {
+        ConfirmationDialog.SetTitle(title);
+        ConfirmationDialog.SetText(text);
+        ConfirmationDialog.SetConfirmAction(confirmAction);
+        ConfirmationDialog.SetCancelAction(cancelAction);
+
+        ConfirmationDialog.gameObject.SetActive(true);
+    }
+
+    public void ShowInputFieldDialog<T>(string title,
+                                       string text,
+                                       UnityAction<T> confirmAction,
+                                       UnityAction<T> cancelAction,
+                                       string cancelText = "Cancel",
+                                       string confirmText = "Confirm")
+    {
+        InputFieldDialog.SetTitle(title);
+        InputFieldDialog.SetText(text);
+        InputFieldDialog.SetConfirmAction<T>(confirmAction);
+        InputFieldDialog.SetCancelAction<T>(cancelAction);
+
+        InputFieldDialog.gameObject.SetActive(true);
     }
 
     /// <summary>
---
+++ game/Assets/Plugins/UMenu Pro/Content/Scripts/Misc/UMP_InputDialogUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class UMP_InputDialogUI : UMP_ConfirmationDialogUI
+{
+    [SerializeField]
+    private InputField inputField;
+
+    public void SetConfirmAction<T>(UnityAction<T> action)
+    {
+        this.confirmButton.onClick.RemoveAllListeners();
+        //this.confirmButton.onClick.AddListener(() => action.Invoke(context));
+        this.confirmButton.onClick.AddListener(Close);
+    }
+
+    public void SetCancelAction<T>(UnityAction<T> action)
+    {
+        this.cancelButton.onClick.RemoveAllListeners();
+        //this.cancelButton.onClick.AddListener(() => action.Invoke(context));
+        this.cancelButton.onClick.AddListener(Close);
+    }
+
+    public void SetInputPlaceholder(ref InputField inputField, string placeholder)
+    {
+        Text placeholderText = (Text)inputField.placeholder;
+        placeholderText.text = placeholder;
+    }
+
+    public string GetInputValue()
+    {
+        return inputField.text;
+    }
+}
