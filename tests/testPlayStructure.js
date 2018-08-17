0747a5ed2bb1daf4b5230118270c51cee4cd5abe
warrenshen <warren.shen@houzz.com>
2018-08-17 16:47:57 -0700

    Write test for play creature in front of structure

--- tests/testPlayStructure.js
+++ tests/testPlayStructure.js
@@ -257,4 +257,212 @@ describe("challenge play structure", function() {
       });
     });
   });
+
+  describe("play creature in front of structure", function() {
+    const challengeStateData = {
+      "current": {
+        "ID_OPPONENT": {
+          "hasTurn": 0,
+          "manaCurrent": 0,
+          "manaMax": 70,
+          "health": 100,
+          "healthMax": 100,
+          "armor": 0,
+          "field": [
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            }
+          ],
+          "fieldBack": [
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+          ],
+          "hand": [],
+          "deckSize": 0,
+          "cardCount": 6,
+          "mode": 0,
+          "mulliganCards": [],
+          "id": "ID_OPPONENT",
+          "expiredStreak": 0
+        },
+        "ID_PLAYER": {
+          "hasTurn": 1,
+          "manaCurrent": 70,
+          "manaMax": 70,
+          "health": 60,
+          "healthMax": 100,
+          "armor": 0,
+          "field": [
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "ID_PLAYER-13",
+              "playerId": "ID_PLAYER",
+              "level": 1,
+              "category": 0,
+              "attack": 10,
+              "health": 30,
+              "cost": 30,
+              "name": "Pricklepillar",
+              "description": "Taunt; Lethal",
+              "abilities": [
+                1,
+                21
+              ],
+              "baseId": "C49",
+              "attackStart": 10,
+              "costStart": 30,
+              "healthStart": 30,
+              "healthMax": 30,
+              "buffs": [],
+              "canAttack": 1,
+              "isFrozen": 0,
+              "isSilenced": 0,
+              "spawnRank": 5,
+            },
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+          ],
+          "fieldBack": [
+            {
+              "id": 'ID_PLAYER-0',
+              "playerId": 'ID_PLAYER',
+              "level": 1,
+              "category": 2,
+              "health": 10,
+              "cost": 20,
+              "name": 'Taunt Structure',
+              "baseId": 'C36',
+              "costStart": 20,
+              "healthStart": 30,
+              "healthMax": 30,
+              "spawnRank": 2
+            },
+            {
+              "id": "EMPTY"
+            },
+            {
+              "id": "EMPTY"
+            },
+          ],
+          "hand": [
+            {
+              "id": "ID_PLAYER-16",
+              "playerId": "ID_PLAYER",
+              "level": 1,
+              "category": 0,
+              "attack": 50,
+              "health": 40,
+              "cost": 50,
+              "name": "Cereboarus",
+              "description": "Lifesteal; Deathwish: Draw a card",
+              "abilities": [
+                5,
+                6,
+              ],
+              "baseId": "C46",
+              "attackStart": 50,
+              "costStart": 50,
+              "healthStart": 40,
+              "healthMax": 40,
+            },
+          ],
+          "deckSize": 0,
+          "deck": [],
+          "cardCount": 8,
+          "mode": 0,
+          "mulliganCards": [],
+          "id": "ID_PLAYER",
+          "expiredStreak": 0
+        },
+      },
+      "opponentIdByPlayerId": {
+        "ID_PLAYER": "ID_OPPONENT",
+        "ID_OPPONENT": "ID_PLAYER",
+      },
+      "lastMoves": [],
+      "moves": [],
+      "expCardIdsByPlayerId": {
+        "ID_PLAYER": [],
+        "ID_OPPONENT": [],
+      },
+      "deadCards": [],
+      "moveTakenThisTurn": 0,
+      "expiredStreakByPlayerId": {
+        "ID_PLAYER": 0,
+        "ID_OPPONENT": 0,
+      },
+      "spawnCount": 2,
+      "deathCount": 0,
+      "nonce": 14
+    };
+
+    it("should support play creature grant taunt", function() {
+      return new Promise((resolve) => {
+        gamesparks.sendWithData(
+          "LogEventRequest",
+          {
+            eventKey: "TestChallengePlayCard",
+            challengeStateString: JSON.stringify(challengeStateData),
+            challengePlayerId: "ID_PLAYER",
+            cardId: "ID_PLAYER-16",
+            attributesJson: {
+              fieldIndex: 0,
+            },
+          },
+          function(response) {
+            const challengeStateData = response.scriptData.challengeStateData;
+
+            const lastMoves = challengeStateData.lastMoves;
+            assert.equal(lastMoves.length, 1);
+            assert.equal(lastMoves[0].category, "MOVE_CATEGORY_PLAY_MINION");
+            assert.equal(lastMoves[0].attributes.cardId, "ID_PLAYER-16");
+            assert.equal(lastMoves[0].attributes.fieldIndex, 0);
+
+            const playerState = challengeStateData.current["ID_PLAYER"];
+            const playerField = playerState.field;
+            assert.equal(playerField[0].id, "ID_PLAYER-16");
+            assert.equal(playerField[0].health, 40);
+            assert.equal(playerField[0].abilities.length, 3);
+            assert.equal(playerField[0].abilities.indexOf(1) >= 0, true);
+
+            resolve();
+          }
+        );
+      });
+    });
+  });
 });
