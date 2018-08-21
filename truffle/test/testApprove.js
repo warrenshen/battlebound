71a1185ce6441e438430b17ad7de65e0aff86c65
warrenshen <warren.shen@houzz.com>
2018-08-21 10:17:49 -0700

    Fix approve test

--- truffle/test/testApprove.js
+++ truffle/test/testApprove.js
@@ -4,12 +4,16 @@ contract('CardTreasury', function(accounts) {
 
   let contract;
 
+  const minter = accounts[0];
+  const buyer = accounts[1];
+  const buyerTwo = accounts[2];
+
   describe ("should nots", function() {
     beforeEach(async function() {
       contract = await CardTreasury.new();
-      await contract.createTemplate(1, 0, 8, "T1", { from: accounts[0] });
-      await contract.createTemplate(1, 0, 9, "T1", { from: accounts[0] });
-      await contract.mintCard(0, accounts[1], { from: accounts[0] });
+      await contract.mintTemplate(1, 0, 0, 8, "T1", { from: minter });
+      await contract.mintTemplate(1, 0, 0, 9, "T1", { from: minter });
+      await contract.mintCard(0, 0, buyer, { from: minter });
     });
 
     it ("should not allow non-owner of card to approve account address", async function() {
@@ -17,9 +21,9 @@ contract('CardTreasury', function(accounts) {
 
       try {
         transaction = await contract.approve(
-          accounts[2],
+          buyerTwo,
           0,
-          { from: accounts[2] }
+          { from: buyerTwo }
         );
       } catch (error) {
         err = error;
@@ -34,7 +38,7 @@ contract('CardTreasury', function(accounts) {
         transaction = await contract.approve(
           contract.address,
           0,
-          { from: accounts[2] }
+          { from: buyerTwo }
         );
       } catch (error) {
         err = error;
@@ -46,20 +50,20 @@ contract('CardTreasury', function(accounts) {
   describe ("shoulds", function() {
     beforeEach(async function() {
       contract = await CardTreasury.new();
-      await contract.createTemplate(1, 0, 9, "T1", { from: accounts[0] });
-      await contract.mintCard(0, accounts[1], { from: accounts[0] });
+      await contract.mintTemplate(1, 0, 0, 9, "T1", { from: minter });
+      await contract.mintCard(0, 0, buyer, { from: minter });
     });
 
     it ("should allow owner of card to approve account address", async function() {
       const transaction = await contract.approve(
-        accounts[2],
+        buyerTwo,
         0,
-        { from: accounts[1] }
+        { from: buyer }
       );
       const log = transaction.logs[0];
       assert.equal(log.event, "Approval", "expected an Approval event");
-      assert.equal(log.args.owner, accounts[1], "Approval event owner is incorrect");
-      assert.equal(log.args.approved, accounts[2], "Approval event approved is incorrect");
+      assert.equal(log.args.owner, buyer, "Approval event owner is incorrect");
+      assert.equal(log.args.approved, buyerTwo, "Approval event approved is incorrect");
       assert.equal(log.args.tokenId, 0, "Approval event token id is incorrect");
     });
 
@@ -67,11 +71,11 @@ contract('CardTreasury', function(accounts) {
       const transaction = await contract.approve(
         contract.address,
         0,
-        { from: accounts[1] }
+        { from: buyer }
       );
       const log = transaction.logs[0];
       assert.equal(log.event, "Approval", "expected an Approval event");
-      assert.equal(log.args.owner, accounts[1], "Approval event owner is incorrect");
+      assert.equal(log.args.owner, buyer, "Approval event owner is incorrect");
       assert.equal(log.args.approved, contract.address, "Approval event approved is incorrect");
       assert.equal(log.args.tokenId, 0, "Approval event token id is incorrect");
     });
