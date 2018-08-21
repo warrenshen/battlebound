2b0a5ad44eebfc80daf38fa2d05b9c6797b9b8fc
warrenshen <warren.shen@houzz.com>
2018-08-21 00:10:32 -0700

    Update tests for card treasury

--- truffle/test/testGetCard.js
+++ truffle/test/testGetCard.js
@@ -4,6 +4,8 @@ contract('CardTreasury', function(accounts) {
 
   let contract;
 
+  const minter = accounts[0];
+
   describe ("should nots", function() {
     before(async function() {
       contract = await CardTreasury.new();
@@ -13,7 +15,7 @@ contract('CardTreasury', function(accounts) {
       let response = null;
 
       try {
-        await contract.getCard.call(0, { from: accounts[0] });
+        await contract.getCard.call(0, { from: minter });
       } catch (error) {
         err = error
       }
@@ -31,11 +33,11 @@ contract('CardTreasury', function(accounts) {
   describe ("shoulds", function() {
     before(async function() {
       contract = await CardTreasury.new();
-      await contract.createTemplate(2, 0, 8, "T1", { from: accounts[0] });
-      await contract.createTemplate(1, 1, 9, "T2", { from: accounts[0] });
-      await contract.mintCard(0, accounts[0], { from: accounts[0] });
-      await contract.mintCard(1, accounts[0], { from: accounts[0] });
-      await contract.mintCard(0, accounts[0], { from: accounts[0] });
+      await contract.mintTemplate(2, 0, 0, 8, "Lux", { from: minter });
+      await contract.mintTemplate(1, 1, 1, 9, "Talusreaver", { from: minter });
+      await contract.mintCard(0, 0, minter, { from: minter });
+      await contract.mintCard(1, 1, minter, { from: minter });
+      await contract.mintCard(0, 1, minter, { from: minter });
     });
 
     it ("should return correct card information", async function() {
@@ -44,26 +46,29 @@ contract('CardTreasury', function(accounts) {
       let power;
       let name;
 
-      response = await contract.getCard.call(0, { from: accounts[0] });
-      [generation, power, name] = response;
+      response = await contract.getCard.call(0, { from: minter });
+      [generation, category, power, name, variation] = response;
       assert.equal(generation, 0, "response generation is incorrect");
+      assert.equal(category, 0, "response category is incorrect");
       assert.equal(power, 8, "response generation is incorrect");
-      assert.equal(name, "T1", "response name is incorrect");
+      assert.equal(name, "Lux", "response name is incorrect");
 
-      response = await contract.getCard.call(1, { from: accounts[0] });
+      response = await contract.getCard.call(1, { from: minter });
       [category, name] = response;
-      [generation, power, name] = response;
+      [generation, category, power, name, variation] = response;
       assert.equal(generation, 1, "response generation is incorrect");
+      assert.equal(category, 1, "response category is incorrect");
       assert.equal(power, 9, "response generation is incorrect");
-      assert.equal(name, "T2", "response name is incorrect");
+      assert.equal(name, "Talusreaver", "response name is incorrect");
 
 
-      response = await contract.getCard.call(2, { from: accounts[0] });
+      response = await contract.getCard.call(2, { from: minter });
       [category, name] = response;
-      [generation, power, name] = response;
+      [generation, category, power, name, variation] = response;
       assert.equal(generation, 0, "response generation is incorrect");
+      assert.equal(category, 0, "response category is incorrect");
       assert.equal(power, 8, "response generation is incorrect");
-      assert.equal(name, "T1", "response name is incorrect");
+      assert.equal(name, "Lux", "response name is incorrect");
 
     });
   });
--- truffle/test/testMintTemplate.js
+++ truffle/test/testMintTemplate.js
@@ -4,15 +4,19 @@ contract('CardTreasury', function(accounts) {
 
   let contract;
 
+  const minter = accounts[0];
+  const buyer = accounts[1];
+
   beforeEach(async function() {
      contract = await CardTreasury.new();
   });
 
-  it ("should allow privileged address to create a template", async function() {
-    const transaction = await contract.createTemplate(1, 0, 9, "T1", { from: accounts[0] });
-    assert.equal(transaction.logs[0].event, "TemplateCreated", "expected a TemplateCreated event");
+  it ("should allow privileged address to mint a template", async function() {
+    const transaction = await contract.mintTemplate(1, 0, 0, 9, "Lux", { from: minter });
+    assert.equal(transaction.logs[0].event, "TemplateMinted");
+    assert.equal(transaction.logs[0].args.templateId, 0);
 
-    const supply = await contract.templatesSupply.call();
+    const supply = await contract.templateSupply.call();
     assert.equal(supply.valueOf(), 1, "supply of templates is not 1");
   });
 
@@ -20,13 +24,13 @@ contract('CardTreasury', function(accounts) {
     let transaction = null;
 
     try {
-      transaction = await contract.createTemplate(1, 0, 9, "T1", { from: accounts[1] });
+      transaction = await contract.mintTemplate(1, 0, 0, 9, "Lux", { from: buyer });
     } catch (error) {
       err = error
     }
     assert.equal(transaction, null, "transaction should not exist");
 
-    const supply = await contract.templatesSupply.call();
+    const supply = await contract.templateSupply.call();
     assert.equal(supply.valueOf(), 0, "supply of templates is not 0");
   });
 
@@ -34,16 +38,18 @@ contract('CardTreasury', function(accounts) {
     let transaction;
     let supply;
 
-    transaction = await contract.createTemplate(1, 0, 9, "T1", { from: accounts[0] });
-    assert.equal(transaction.logs[0].event, "TemplateCreated", "expected a TemplateCreated event");
+    transaction = await contract.mintTemplate(1, 0, 0, 9, "Lux", { from: minter });
+    assert.equal(transaction.logs[0].event, "TemplateMinted");
+    assert.equal(transaction.logs[0].args.templateId, 0);
 
-    supply = await contract.templatesSupply.call();
+    supply = await contract.templateSupply.call();
     assert.equal(supply.valueOf(), 1, "supply of templates is not 1");
 
-    transaction = await contract.createTemplate(1, 0, 9, "T2", { from: accounts[0] });
-    assert.equal(transaction.logs[0].event, "TemplateCreated", "expected a TemplateCreated event");
+    transaction = await contract.mintTemplate(1, 0, 0, 9, "Talusreaver", { from: minter });
+    assert.equal(transaction.logs[0].event, "TemplateMinted");
+    assert.equal(transaction.logs[0].args.templateId, 1);
 
-    supply = await contract.templatesSupply.call();
+    supply = await contract.templateSupply.call();
     assert.equal(supply.valueOf(), 2, "supply of templates is not 2");
   });
 
@@ -51,13 +57,13 @@ contract('CardTreasury', function(accounts) {
     let transaction = null;
 
     try {
-      transaction = await contract.createTemplate(0, 0, 9, "T1", { from: accounts[0] });
+      transaction = await contract.mintTemplate(0, 0, 0, 9, "Lux", { from: minter });
     } catch (error) {
       err = error
     }
     assert.equal(transaction, null, "transaction should not exist");
 
-    const supply = await contract.templatesSupply.call();
+    const supply = await contract.templateSupply.call();
     assert.equal(supply.valueOf(), 0, "supply of templates is not 0");
   });
 
@@ -65,11 +71,17 @@ contract('CardTreasury', function(accounts) {
     let transaction;
     let limit;
 
-    transaction = await contract.createTemplate(3, 0, 9, "T1", { from: accounts[0] });
+    transaction = await contract.mintTemplate(3, 0, 0, 9, "Lux", { from: minter });
+    assert.equal(transaction.logs[0].event, "TemplateMinted");
+    assert.equal(transaction.logs[0].args.templateId, 0);
+
     limit = await contract.instanceLimit.call(0);
     assert.equal(limit.toNumber(), 3, "instance limit is not 3");
 
-    transaction = await contract.createTemplate(8, 0, 9, "T2", { from: accounts[0] });
+    transaction = await contract.mintTemplate(8, 0, 0, 9, "Talusreaver", { from: minter });
+    assert.equal(transaction.logs[0].event, "TemplateMinted");
+    assert.equal(transaction.logs[0].args.templateId, 1);
+
     limit = await contract.instanceLimit.call(1);
     assert.equal(limit.toNumber(), 8, "instance limit is not 8");
 
