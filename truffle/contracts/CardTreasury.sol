d34ea4e09d20e0626b108280bdce3fea48579c64
warrenshen <warren.shen@houzz.com>
2018-08-21 00:10:16 -0700

    Finalize card treasury contract

--- truffle/contracts/CardTreasury.sol
+++ truffle/contracts/CardTreasury.sol
@@ -35,66 +35,65 @@ contract ClockAuction {
 //   function name() public view returns (string);
 //   function symbol() public view returns (string);
 //   function tokensOfOwner(address _owner) external view returns (uint256[] tokenIds);
-//   // function tokenMetadata(uint256 _tokenId, string _preferredTransport) public view returns (string infoUrl);
+  // function tokenMetadata(uint256 _tokenId, string _preferredTransport) public view returns (string infoUrl);
 
 //   // ERC-165 Compatibility (https://github.com/ethereum/EIPs/issues/165)
 //   // function supportsInterface(bytes4 _interfaceID) external view returns (bool);
 // }
 
 contract CardBase is Pausable {
-    // bytes4 constant InterfaceSignature_ERC165 =
-    //     bytes4(keccak256('supportsInterface(bytes4)'));
+  bytes4 constant InterfaceSignature_ERC165 =
+    bytes4(keccak256("supportsInterface(bytes4)"));
 
-    // bytes4 constant InterfaceSignature_ERC721 =
-    //     bytes4(keccak256('name()')) ^
-    //     bytes4(keccak256('symbol()')) ^
-    //     bytes4(keccak256('totalSupply()')) ^
-    //     bytes4(keccak256('balanceOf(address)')) ^
-    //     bytes4(keccak256('ownerOf(uint256)')) ^
-    //     bytes4(keccak256('approve(address,uint256)')) ^
-    //     bytes4(keccak256('transfer(address,uint256)')) ^
-    //     bytes4(keccak256('transferFrom(address,address,uint256)')) ^
-    //     bytes4(keccak256('tokensOfOwner(address)')) ^
-    //     bytes4(keccak256('tokenMetadata(uint256,string)'));
+  bytes4 constant InterfaceSignature_ERC721 =
+    bytes4(keccak256("name()")) ^
+    bytes4(keccak256("symbol()")) ^
+    bytes4(keccak256("totalSupply()")) ^
+    bytes4(keccak256("balanceOf(address)")) ^
+    bytes4(keccak256("ownerOf(uint256)")) ^
+    bytes4(keccak256("approve(address,uint256)")) ^
+    bytes4(keccak256("transfer(address,uint256)")) ^
+    bytes4(keccak256("transferFrom(address,address,uint256)")) ^
+    bytes4(keccak256("tokensOfOwner(address)")) ^
+    bytes4(keccak256("tokenMetadata(uint256,string)"));
 
-    // /// @notice Introspection interface as per ERC-165 (https://github.com/ethereum/EIPs/issues/165).
-    // ///  Returns true for any standardized interfaces implemented by this contract. We implement
-    // ///  ERC-165 (obviously!) and ERC-721.
-    // function supportsInterface(bytes4 _interfaceID) external view returns (bool)
-    // {
-    //     // DEBUG ONLY
-    //     //require((InterfaceSignature_ERC165 == 0x01ffc9a7) && (InterfaceSignature_ERC721 == 0x9a20483d));
+  /// @notice Introspection interface as per ERC-165 (https://github.com/ethereum/EIPs/issues/165).
+  ///  Returns true for any standardized interfaces implemented by this contract. We implement
+  ///  ERC-165 (obviously!) and ERC-721.
+  function supportsInterface(bytes4 _interfaceID) external view returns (bool)
+  {
+      // DEBUG ONLY
+      //require((InterfaceSignature_ERC165 == 0x01ffc9a7) && (InterfaceSignature_ERC721 == 0x9a20483d));
 
-    //     return ((_interfaceID == InterfaceSignature_ERC165) || (_interfaceID == InterfaceSignature_ERC721));
-    // }
+      return ((_interfaceID == InterfaceSignature_ERC165) || (_interfaceID == InterfaceSignature_ERC721));
+  }
 }
 
 contract CardMint is CardBase {
 
   /* EVENTS */
-  event TemplateCreated(uint256 templateId);
-  event InstanceMinted(address owner, uint256 cardId, uint256 templateId);
+  event TemplateMinted(uint256 templateId);
+  event CardMinted(address owner, uint256 cardId);
 
   event Transfer(address indexed from, address indexed to, uint256 tokenId);
   event Approval(address owner, address approved, uint256 tokenId);
 
   /* DATA TYPES */
   struct Template {
-    uint64 generation;
-    // uint64 category;
+    uint128 generation;
+    uint64 category;
     uint64 power;
     string name;
   }
 
   struct Card {
     uint256 templateId;
-    uint256 attributeId;
+    uint256 variation;
   }
 
   /* STORAGE */
   Template[] internal templates;
-  // Each uint256 in `cards` is a template ID -
-  // a template ID is simply the index of a template in `templates`.
+  // Each Card is a template ID (index of a template in `templates`) and a variation.
   Card[] internal cards;
 
   // Template ID => max number of cards that can be minted with this template ID.
@@ -125,10 +124,27 @@ contract CardMint is CardBase {
     emit Transfer(_from, _to, _tokenId);
   }
 
+  function _mintCard(uint256 _templateId, uint256 variation, address _owner) internal returns (uint256) {
+    require(templateIdToMintCount[_templateId] < templateIdToMintLimit[_templateId]);
+    templateIdToMintCount[_templateId] = templateIdToMintCount[_templateId] + 1;
+
+    Card memory newCard = Card({
+      templateId: _templateId,
+      variation: 0
+    });
+
+    uint256 newCardId = cards.push(newCard) - 1;
+    _transfer(0, _owner, newCardId);
+
+    emit CardMinted(_owner, newCardId);
+    return newCardId;
+  }
+
   /** PUBLIC FUNCTIONS **/
-  function createTemplate(
+  function mintTemplate(
     uint256 _mintLimit,
-    uint64 _generation,
+    uint128 _generation,
+    uint64 _category,
     uint64 _power,
     string _name
   ) external onlyOwner returns (uint256) {
@@ -136,31 +152,34 @@ contract CardMint is CardBase {
 
     Template memory newTemplate = Template({
       generation: _generation,
+      category: _category,
       power: _power,
       name: _name
     });
     uint256 newTemplateId = templates.push(newTemplate) - 1;
     templateIdToMintLimit[newTemplateId] = _mintLimit;
 
-    emit TemplateCreated(newTemplateId);
+    emit TemplateMinted(newTemplateId);
     return newTemplateId;
   }
 
-  function mintCard(uint256 _templateId, address _owner) external onlyOwner returns (uint256) {
-    require(templateIdToMintCount[_templateId] < templateIdToMintLimit[_templateId]);
-    // need safe math
-    templateIdToMintCount[_templateId] = templateIdToMintCount[_templateId] + 1;
+  function mintCard(uint256 _templateId, uint256 _variation, address _owner) external onlyOwner returns (uint256) {
+    require(_owner != address(0));
+    return _mintCard(_templateId, _variation, _owner);
+  }
 
-    Card memory newCard = Card({
-      templateId: _templateId,
-      attributeId: 0
-    });
+  function mintCards(uint256[] _templateIds, uint256[] _variations, address _owner) external onlyOwner {
+    require(_owner != address(0));
+    require(_templateIds.length == _variations.length);
 
-    uint256 newCardId = cards.push(newCard) - 1;
-    _transfer(0, _owner, newCardId);
+    uint256 templateId;
+    uint256 variation;
 
-    emit InstanceMinted(_owner, newCardId, _templateId);
-    return newCardId;
+    for (uint256 i = 0; i < _templateIds.length; ++i) {
+      templateId = _templateIds[i];
+      variation = _variations[i];
+      _mintCard(templateId, variation, _owner);
+    }
   }
 }
 
@@ -218,7 +237,8 @@ contract CardTreasury is CardOwnership {
     view
     returns (
       uint128 generation,
-      uint128 power,
+      uint64 category,
+      uint64 power,
       string name
     )
   {
@@ -227,6 +247,7 @@ contract CardTreasury is CardOwnership {
     Template storage template = templates[_templateId];
 
     generation = template.generation;
+    category = template.category;
     power = template.power;
     name = template.name;
   }
@@ -236,8 +257,10 @@ contract CardTreasury is CardOwnership {
     view
     returns (
       uint128 generation,
-      uint128 power,
-      string name
+      uint64 category,
+      uint64 power,
+      string name,
+      uint256 variation
     )
   {
     require(_cardId < cards.length);
@@ -246,8 +269,10 @@ contract CardTreasury is CardOwnership {
     Template storage template = templates[card.templateId];
 
     generation = template.generation;
+    category = template.category;
     power = template.power;
     name = template.name;
+    variation = card.variation;
   }
 
   function templateIdOf(uint256 _tokenId) external view returns (uint256) {
@@ -265,24 +290,24 @@ contract CardTreasury is CardOwnership {
     return ownerCardCount[_owner];
   }
 
-  function templatesSupply() public view returns (uint256) {
+  function templateSupply() external view returns (uint256) {
     return templates.length;
   }
 
-  function instancesSupply() public view returns (uint256) {
+  function totalSupply() external view returns (uint256) {
     return cards.length;
   }
 
-  function instanceLimit(uint256 _templateId) public view returns(uint256) {
+  function instanceLimit(uint256 _templateId) external view returns(uint256) {
     return templateIdToMintLimit[_templateId];
   }
 
-  function name() public pure returns (string) {
-    return "TCG";
+  function name() external pure returns (string) {
+    return "Battlebound";
   }
 
-  function symbol() public pure returns (string) {
-    return "TCG";
+  function symbol() external pure returns (string) {
+    return "BB";
   }
 
   function tokensOfOwner(address _owner) external view returns (uint256[]) {
@@ -331,4 +356,8 @@ contract CardTreasury is CardOwnership {
         msg.sender
     );
   }
+
+  // function tokenURI(uint256 _tokenId) external pure returns (string) {
+  //   return
+  // }
 }
