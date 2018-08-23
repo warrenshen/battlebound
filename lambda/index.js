d42e90af0a5d2862834cb5e1d2dc22240e72ca8a
warrenshen <warren.shen@houzz.com>
2018-08-23 01:32:46 -0700

    Update lambda to sign and return mint cards function

--- lambda/index.js
+++ lambda/index.js
@@ -1,47 +1,60 @@
-exports.handler = async (event) => {
-    var nonce = event.nonce;
-    console.log('nonce is ', event.nonce);
-    console.log('templateId is ', event.templateId);
-    console.log('owner address is ', event.ownerAddress);
-    if (!nonce) {
-        return 'Nonce param is not set';
-    }
+exports.handler = async (event, callback) => {
+  const nonce = event.nonce;
+  const gasPriceInGWei = event.gasPrice;
+  const gasLimit = event.gasLimit;
+  const chainId = event.chainId;
+  const contractAddress = event.contractAddress;
+  const privateKey = event.privateKey;
 
-    var Web3 = require("web3-js").Web3;
-    var EthTx = require("ethereumjs-tx");
+  if (!nonce || !gasPriceInGWei || !gasLimit) {
+    return "ERROR: invalid nonce, gasPrice, or gasLimit param";
+  } else if (gasPriceInGWei > 10) {
+    return "ERROR: gasPrice param should not be greater than 10";
+  } else if (!chainId ||chainId != 4 && chainId != 1) {
+    return "ERROR: invalid chainId param";
+  } else if (!contractAddress || contractAddress.indexOf("0x") != 0 || contractAddress.length != 42) {
+    return "ERROR: invalid contractAddress param";
+  } else if (!privateKey || privateKey.indexOf("0x") != 0 || privateKey.length != 66) {
+    return "ERROR: invalid privateKey param";
+  }
 
-    var contractAddress = "0x5Cd6BaEF2B5bd80CD699A702649591DC1fE6bf33";
-    var contractAbi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"instancesSupply","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_templateId","type":"uint256"}],"name":"getTemplate","outputs":[{"name":"generation","type":"uint128"},{"name":"power","type":"uint128"},{"name":"name","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"templatesSupply","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_templateId","type":"uint256"}],"name":"instanceLimit","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"name":"owner","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"tokensOfOwner","outputs":[{"name":"tokenIds","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_cardId","type":"uint256"}],"name":"getCard","outputs":[{"name":"generation","type":"uint128"},{"name":"power","type":"uint128"},{"name":"name","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_templateId","type":"uint256"},{"name":"_owner","type":"address"}],"name":"mintCard","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_mintLimit","type":"uint256"},{"name":"_generation","type":"uint128"},{"name":"_power","type":"uint128"},{"name":"_name","type":"string"}],"name":"createTemplate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"templateId","type":"uint256"}],"name":"TemplateCreated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"owner","type":"address"},{"indexed":false,"name":"cardId","type":"uint256"},{"indexed":false,"name":"templateId","type":"uint256"}],"name":"InstanceMinted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_owner","type":"address"},{"indexed":false,"name":"_approved","type":"address"},{"indexed":false,"name":"_tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"}],"name":"OwnershipRenounced","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}];
+  const templateIds = event.templateIds;
+  const variations = event.variations;
+  const recipientAddress = event.recipientAddress;
 
-    var ownerAddress = "0x2afaf114b6833b951a00610cededd3d7c7a48ae8";
-    var ownerPrivateKey = Buffer.from(
-      "763f4cac19b185ef4d08bea734541655bc38c043f179a177ae83bda945b86a16",
-      "hex"
-    );
+  if (!Array.isArray(templateIds) || !Array.isArray(variations)) {
+    return "ERROR: invalid templateIds or variations param";
+  } else if (templateIds.length != variations.length) {
+    return "ERROR: params templateIds and variations must have same length";
+  } else if (!recipientAddress || recipientAddress.indexOf("0x") != 0 || recipientAddress.length != 42) {
+    return "ERROR: invalid recipientAddress param";
+  }
 
-    var web3 = new Web3(new Web3.providers.HttpProvider(
-      "https://rinkeby.infura.io/kBLFY7NMU7NrFMdpvDR8"
-    ));
+  const Web3 = require("web3-js").Web3;
+  const web3 = new Web3();
+  // const contractAddress = "0x948d395aA9Bafb8C819F9A5EC59f36b8E92E375B";
+  const contractAbi = [{"constant":true,"inputs":[{"name":"_interfaceID","type":"bytes4"}],"name":"supportsInterface","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"minter","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"templateSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_templateId","type":"uint256"}],"name":"getTemplate","outputs":[{"name":"generation","type":"uint128"},{"name":"category","type":"uint64"},{"name":"power","type":"uint64"},{"name":"name","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_templateIds","type":"uint256[]"},{"name":"_variations","type":"uint256[]"},{"name":"_owner","type":"address"}],"name":"mintCards","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_tokenId","type":"uint256"},{"name":"_startingPrice","type":"uint256"},{"name":"_endingPrice","type":"uint256"},{"name":"_duration","type":"uint256"}],"name":"createSaleAuction","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"exists","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_templateId","type":"uint256"}],"name":"instanceLimit","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_mintLimit","type":"uint256"},{"name":"_generation","type":"uint128"},{"name":"_category","type":"uint64"},{"name":"_power","type":"uint64"},{"name":"_name","type":"string"}],"name":"mintTemplate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"tokensOfOwner","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_cardId","type":"uint256"}],"name":"getCard","outputs":[{"name":"generation","type":"uint128"},{"name":"category","type":"uint64"},{"name":"power","type":"uint64"},{"name":"name","type":"string"},{"name":"variation","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"_operator","type":"address"},{"name":"_approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"}],"name":"setSaleAuction","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_tokenId","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_templateId","type":"uint256"},{"name":"_variation","type":"uint256"},{"name":"_owner","type":"address"}],"name":"mintCard","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_cardId","type":"uint256"}],"name":"templateIdOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"saleAuction","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_minter","type":"address"}],"name":"setMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_templateId","type":"uint256"}],"name":"TemplateMinted","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_owner","type":"address"},{"indexed":false,"name":"_cardId","type":"uint256"}],"name":"CardMinted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":true,"name":"_tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_approved","type":"address"},{"indexed":true,"name":"_tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_operator","type":"address"},{"indexed":false,"name":"_approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"}],"name":"OwnershipRenounced","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}];
 
-    var data = new web3.eth.Contract(contractAbi, contractAddress)
-          .methods
-          .mintCard(0, "0x2aFAf114B6833B951A00610cEdeDD3D7c7A48AE8")
-          .encodeABI();
+  const data = new web3.eth.Contract(contractAbi, contractAddress)
+    .methods
+    .mintCards(
+      templateIds,
+      variations,
+      recipientAddress
+    )
+    .encodeABI();
 
-    var txParams = {
-      nonce: web3.utils.toHex(nonce),
-      gasPrice: web3.utils.toHex(web3.utils.toWei('5', 'gwei')),
-      gasLimit: 1000000,
-      to: contractAddress,
-      value: 0,
-      data: data,
-      chainId: 4,
-    };
+  const txParams = {
+    nonce: web3.utils.toHex(nonce),
+    gasPrice: web3.utils.toHex(web3.utils.toWei(gasPriceInGWei.toString(), 'gwei')),
+    gasLimit: web3.utils.toHex(gasLimit),
+    to: contractAddress,
+    value: web3.utils.toHex(0),
+    data: data,
+    chainId: chainId,
+  };
 
-    var tx = new EthTx(txParams);
-    tx.sign(ownerPrivateKey);
-
-    var rawTx = '0x' + tx.serialize().toString('hex');
-    return 'This is cool, hello, this is Bob!';
-
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+  const signedTx = await account.signTransaction(txParams)
+  return signedTx.rawTransaction;
 };
