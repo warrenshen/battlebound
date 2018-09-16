058c3801c7f44525ddd5dc9cf9cf479834500e06
warrenshen <warren.shen@houzz.com>
2018-09-16 16:46:15 -0700

    Fix developer panel to show current flags and support use staging flag

--- game/Assets/Scripts/Editor/DeveloperPanel.cs
+++ game/Assets/Scripts/Editor/DeveloperPanel.cs
@@ -1,21 +1,17 @@
 ﻿#if UNITY_EDITOR
-using System.Collections;
-using System.Collections.Generic;
-
 using UnityEngine;
 using UnityEditor;
 
 public class DeveloperPanel : EditorWindow
 {
+    private bool isInitialized;
     private bool flagsFoldout;
+
     private bool useServer;
+    private bool useStaging;
     private bool logVerbose;
     private bool skipMulligan;
     private bool showChinese;
-
-    public const string FLAG_USE_SERVER = "FLAG_USE_SERVER";
-    public const string FLAG_LOG_VERBOSE = "FLAG_LOG_VERBOSE";
-    public const string FLAG_SKIP_MULLIGAN = "FLAG_SKIP_MULLIGAN";
 
     [MenuItem("Custom/Developer Panel")]
     static void Init()
@@ -23,22 +19,29 @@ public class DeveloperPanel : EditorWindow
         // Get existing open window or if none, make a new one:
         DeveloperPanel window = EditorWindow.GetWindow(typeof(DeveloperPanel)) as DeveloperPanel;
         window.minSize = new Vector2(100, 100);
-        window.flagsFoldout = true;
-        window.useServer = FlagHelper.GetFlag(FLAG_USE_SERVER);
-        window.logVerbose = FlagHelper.GetFlag(FLAG_LOG_VERBOSE);
-        window.skipMulligan = FlagHelper.GetFlag(FLAG_SKIP_MULLIGAN);
-        window.showChinese = FlagHelper.GetFlag("LANGUAGE");
         window.Show();
     }
 
     private void OnGUI()
     {
-        EditorGUILayout.BeginVertical();
+        if (!this.isInitialized)
+        {
+            this.flagsFoldout = true;
+            this.useServer = FlagHelper.GetFlag(FlagHelper.FLAG_USE_SERVER);
+            this.useStaging = FlagHelper.GetFlag(FlagHelper.FLAG_USE_STAGING);
+            this.logVerbose = FlagHelper.GetFlag(FlagHelper.FLAG_LOG_VERBOSE);
+            this.skipMulligan = FlagHelper.GetFlag(FlagHelper.FLAG_SKIP_MULLIGAN);
+            this.showChinese = FlagHelper.GetFlag("LANGUAGE");
 
-        this.flagsFoldout = EditorGUILayout.Foldout(this.flagsFoldout, "Development Flags");
-        if (flagsFoldout)
+            this.isInitialized = true;
+        }
+
+        EditorGUILayout.BeginVertical();
+        this.flagsFoldout = EditorGUILayout.Foldout(this.flagsFoldout, "Development Flags", true);
+        if (this.flagsFoldout)
         {
             this.useServer = EditorGUILayout.Toggle("Use server", this.useServer);
+            this.useStaging = EditorGUILayout.Toggle("Use staging", this.useStaging);
             this.logVerbose = EditorGUILayout.Toggle("Log verbose", this.logVerbose);
             this.skipMulligan = EditorGUILayout.Toggle("Skip mulligan", this.skipMulligan);
             this.showChinese = EditorGUILayout.Toggle("Show chinese", this.showChinese);
@@ -54,15 +57,11 @@ public class DeveloperPanel : EditorWindow
 
     private void SaveSettings()
     {
-        int useServerValue = this.useServer ? 1 : 0;
-        int logVerboseValue = this.logVerbose ? 1 : 0;
-        int skipMulliganValue = this.skipMulligan ? 1 : 0;
-        int selectedLangauge = this.showChinese ? 1 : 0;
-
-        PlayerPrefs.SetInt(FLAG_USE_SERVER, useServerValue);
-        PlayerPrefs.SetInt(FLAG_LOG_VERBOSE, logVerboseValue);
-        PlayerPrefs.SetInt(FLAG_SKIP_MULLIGAN, skipMulliganValue);
-        PlayerPrefs.SetInt("LANGUAGE", selectedLangauge);
+        PlayerPrefs.SetInt(FlagHelper.FLAG_USE_SERVER, this.useServer ? 1 : 0);
+        PlayerPrefs.SetInt(FlagHelper.FLAG_USE_STAGING, this.useStaging ? 1 : 0);
+        PlayerPrefs.SetInt(FlagHelper.FLAG_LOG_VERBOSE, this.logVerbose ? 1 : 0);
+        PlayerPrefs.SetInt(FlagHelper.FLAG_SKIP_MULLIGAN, this.skipMulligan ? 1 : 0);
+        PlayerPrefs.SetInt("LANGUAGE", this.showChinese ? 1 : 0);
 
         //done getting values, now save
         PlayerPrefs.Save();
